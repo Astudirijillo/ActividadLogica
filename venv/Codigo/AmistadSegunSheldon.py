eafbb129f88d257540517b5432fd7e410f76320a
katob <labotsirc.1997@gmail.com>
2020-06-09 21:31:19 -0400

    Actualizacion clase 09-06

--- venv/Codigo/AmistadSegunSheldon.py
+++ venv/Codigo/AmistadSegunSheldon.py
@@ -1,34 +1,34 @@
 print("Hacer llamada desde mi casa")
 opcion = input("Contestan llamada? [S/N] ")
-if opcion.upper == "S":
+if opcion.upper() == "S":
     opcion = input("Preguntar ¿Quisiera compartid una merienda conmigo? [S/N] ")
-    if opcion.upper == "S":
+    if opcion.upper() == "S":
         print("Cenar juntos")
         print("Empezar una amistad")
-    elif opcion.upper == "N":
+    elif opcion.upper() == "N":
         opcion = input("Preguntar ¿Disfrutarias de una bebida caliente? [S/N] ")
-        if opcion.upper == "S":
+        if opcion.upper() == "S":
             print("Seleccionar el tipo de bebida que tomaran")
             bebida = input("Te, cafe o cocoa: ")
-            if opcion.lower == "te":
+            if opcion.lower() == "te":
                 print("Tomar té")
                 print("Iniciar una amistad")
-            elif opcion.lower == "cocoa":
+            elif opcion.lower() == "cocoa":
                 print("Tomar té")
                 print("Iniciar una amistad")
-            elif opcion.lower == "cafe":
+            elif opcion.lower() == "cafe":
                 print("Tomar cafe")
                 print("Iniciar una amistad")
-        elif opcion.upper == "N":
+        elif opcion.upper() == "N":
             print("Rebatir")
             opcion = input("Alguna actividad recreacional? (dile uno de tus intereses) [S/N] ")
-            if opcion.lower == "S":
+            if opcion.upper() == "S":
                 print("Comparto un mismo interes")
                 print("Preguntar ¿Porque no compartir eso juntos?")
-            elif opcion.lower == "N":
+            elif opcion.upper() == "N":
                 print("Rebatir")
-                print("Volver a preguntar por una actividad recreacional menos objetable")
-elif opcion.upper == "N":
+                print("Volver a preguntar por una acccion menos objetable")
+elif opcion.upper() == "N":
     print("Dejar un mensaje")
     print("Esperar que devuelva a llamar")
 
