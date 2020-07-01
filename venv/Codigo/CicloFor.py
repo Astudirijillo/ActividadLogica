1c87b26d6097033e399953df29dd8477dce03245
katob <labotsirc.1997@gmail.com>
2020-06-30 21:01:48 -0400

    Avance clase 30-06 (Inclusion ciclo for)

---
+++ venv/Codigo/CicloFor.py
@@ -0,0 +1,29 @@
+def funcionFor():
+    print("Funcion for")
+    #for variable in elemento_Iterable (lista):
+    for i in "ISRAEL":
+        print("i:",i)
+        print(f"dentro de for {i}")
+
+#Creamos una lista
+lista_uno =['a','b','c','d']
+#llamado a la funcion
+#funcionFor()
+
+# var = int(input("Cual es la cantidad de veces: "))
+# for i in range(var):
+#     print("i en range: ",i)
+
+# for i in range(4,7):
+#     print("i en range: ",i)
+
+var1 = int(input("Tamaño de la lista? "))
+lista = []
+contador = 0
+for x in range(var1):
+    print("x y contador", x, contador)
+    # lista[contador]= 1+contador # asignacion
+    contador = 1 +contador
+    lista.append(1+contador)
+
+print("tamaño lista es: ",len(lista))
